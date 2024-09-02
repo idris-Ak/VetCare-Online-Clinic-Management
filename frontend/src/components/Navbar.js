9a27cd83d53b90a6fd6bf285b97f5377b5afe24a
Abdullah Abdosh <s4007967@student.rmit.edu.au>
2024-09-02 21:11:25 +1000

    Complete Project Structure

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,23 @@
+// src/components/Navbar.js
+import React from 'react';
+import './Navbar.css';  // Import the CSS file for styling
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <a href="/" className="navbar-link">Home</a>
+        </li>
+        <li className="navbar-item">
+          <a href="/about" className="navbar-link">About</a>
+        </li>
+        <li className="navbar-item">
+          <a href="/contact" className="navbar-link">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to My React App</h1>
+      <p>This is a basic home page.</p>
+    </div>
+  );
+}
+
+export default HomePage;
