d3dc57c9a9d4c29f2d3f0292d31a5c4245954472
idris-Ak <132613240+idris-Ak@users.noreply.github.com>
2025-05-28 14:41:30 +1000

    Refactor Dockerfile and update dependencies; configure CORS settings with dynamic origin

--- backend/src/main/java/au/edu/rmit/sept/webapp/WebConfig.java
+++ backend/src/main/java/au/edu/rmit/sept/webapp/WebConfig.java
@@ -1,17 +1,22 @@
 package au.edu.rmit.sept.webapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600);
